b0a5faa885b1f7bdae3d32c4341001f7a1815bae
easyas123l1 <andrew.wilson90@hotmail.com>
2020-05-18 20:30:46 -0500

    complete linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    high = len(arr)-1
+    low = 0
+    while True: 
+        middle = (high + low) // 2
+        if arr[middle] == target:
+            return middle
+        if high==low: 
+            return -1
+        if arr[middle] > target:
+            low = middle 
+        else:
+            high = middle 
+    return -1
 
-    # Your code here
 
-
-    return -1  # not found
